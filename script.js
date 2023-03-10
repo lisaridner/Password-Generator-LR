62207bfa037cdba841296b65a511dfae2607d89a
Lisa Ridner <125208782+lisaridner@users.noreply.github.com>
2023-03-09 21:15:59 -0500

    added lowercase and uppercase letters to the array variables

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ var choiceArr = [];
 
 var specialCharArr = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`]
 var numberArr = [`1`,`2`, `3`,`4`, `5`, `6`, `7`, `8`, `9`, `0`]
-var lowerCaseArr = []
-var upperCaseArr = []
+var lowerCaseArr = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y``z` ]
+var upperCaseArr = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L``M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`]
 
