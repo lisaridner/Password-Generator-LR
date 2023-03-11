1b0626c8f95c4a3c4b3b22ae4874e8dee07a93e2
Lisa Ridner <125208782+lisaridner@users.noreply.github.com>
2023-03-10 19:44:29 -0500

    finished the loop and troubleshooted till successfully generating a password

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-I created a password generator that lets the user choose a password that has a minimum of 8 characters and max of 128 characters. I created the chocie arrays, wrote if statements for each, and added prompts for the user to use while creating their password. 
+I created a password generator that lets the user choose a password that has a minimum of 8 characters and max of 128 characters. I created the chocie arrays, wrote if statements for each, and added prompts for the user to use while creating their password. Next, created a loop. Troubleshooted the loop. Tested throughout each step unitl successful. I chose to let the password stay written to the page. 
+
+
+*I worked with a study group on this project*
 
 
 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+
+
+
--- script.js
+++ script.js
@@ -4,20 +4,35 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var characterLength = 8; 
+var choiceArr = [];
+
+var specialCharArr = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`];
+var numberArr = [`1`,`2`, `3`,`4`, `5`, `6`, `7`, `8`, `9`, `0`];
+var lowerCaseArr = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
+var upperCaseArr = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
 
 // Write password to the #password input
 function writePassword() {
-  getPrompts();
+ var correctPrompts = getPrompts();
+ var passwordText = document.querySelector("#password");
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+ if(correctPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+  } else {
+    passwordTest.value = "";
+  }
 
 }
 
 function generatePassword() {
-
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+      var randomIndex = Math.floor(Math.random() * choiceArr.length);
+      password = password + choiceArr[randomIndex];
+  }
+  return password;
 }
 
 function getPrompts() {
@@ -44,12 +59,4 @@ function getPrompts() {
 }
 
 
-
-var characterLenght = 8; 
-var choiceArr = [];
-
-var specialCharArr = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`];
-var numberArr = [`1`,`2`, `3`,`4`, `5`, `6`, `7`, `8`, `9`, `0`];
-var lowerCaseArr = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
-var upperCaseArr = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
 
